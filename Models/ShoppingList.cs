44061b7c826b09b75d3106fa3d2dc7102183a53d
Nicolas Betti Macedo <nickbmcoder@gmail.com>
2026-02-10 01:47:40 -0300

    refactor: make ShoppingList _items list readonly

--- Models/ShoppingList.cs
+++ Models/ShoppingList.cs
@@ -5,7 +5,8 @@ namespace To_Do_List_Manager.Models
 {
     internal class ShoppingList(string title, string description, DateTime? dueDate) : ToDoItem(title, description, dueDate), IItemList
     {
-        private List<ShoppingItem> _items = [];
+        private readonly List<ShoppingItem> _items = [];
         public IReadOnlyCollection<ShoppingItem> Items => _items;
+        
     }
 }
